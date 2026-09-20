96f0abce187211a1b44aacc9df20c7c121483c78
shansshe <shannonsshe@gmail.com>
2026-09-20 11:04:30 +0800

    update #70_climbing_stairs

--- 70_climbing-stairs.cpp
+++ 70_climbing-stairs.cpp
@@ -39,12 +39,32 @@ int stairs(int n) {
     }
 }
 
+// 2. DP
+int stairs_dp(int n){
+    if (n == 1){
+        return 1;
+    }
+    if (n == 2){
+        return 2;
+    }
+    
+    vector <int> dp(n + 1);
+
+    dp[1] = 1;
+    dp[2] = 2;
+
+    for (int i = 3; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+
+    return dp[n];
+}
+
 int main() {
     int n;
     cout << "n:";
     cin >> n;
-    cout << stairs(n) << "\n";
+    // cout << stairs(n) << "\n";
+    cout << stairs_dp(n) << "\n";
     return 0;
 }
-
-// 2. DP
